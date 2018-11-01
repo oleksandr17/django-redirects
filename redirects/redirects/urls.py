9689a45599b0ccea6b9474227b099a66e01c11d8
Oleksandr <oleksandr@elements.nl>
2018-11-01 08:42:28 +0100

    More redirect examples

--- redirects/redirects/urls.py
+++ redirects/redirects/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from .views import redirect_view
+from .views import redirect_success_view, redirect_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('redirect/', redirect_view)
+    path('redirect/', redirect_view),
+    path('redirect-success/', redirect_success_view, name='redirect-success'),
 ]
--- redirects/redirects/views.py
+++ redirects/redirects/views.py
@@ -1,6 +1,26 @@
+from django.http import HttpResponse
 from django.shortcuts import redirect
 
 
-def redirect_view(request):
-    response = redirect('/redirect-success/')
-    return response
+def redirect_view(request):    
+    ### Use response class
+    # response = HttpResponse(status=302)
+    # response['Location'] = '/redirect/success/'
+    # return response
+
+    ### Use response redirect class
+    # return HttpResponseRedirect('/redirect/success/')
+
+    ### Use model's `get_absolute_url()` method
+    # product = Product.objects.filter(featured=True).first()
+    # return redirect(product)
+
+    ### Use url name
+    # return redirect('redirect-success')
+
+    ### Use url
+    return redirect('/redirect-success/')
+
+
+def redirect_success_view(request):
+    return HttpResponse("Redirect success")
